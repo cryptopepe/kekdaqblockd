33870e333ee9aedb4222d020a265714483666078
kekdaq <info@kekdaq.com>
2018-08-28 17:27:18 -0400

    Add method

--- lib/api.py
+++ lib/api.py
@@ -1187,6 +1187,16 @@ def serve_api(mongo_db, redis_client):
         return len(siofeeds.onlineClients)
 
     @dispatcher.add_method
+    def get_who_online():
+      w = []
+      l = list(mongo_db.chat_handles.find({}, {'_id': 0}).sort("when", pymongo.DESCENDING))
+            for x in l:
+                if x['wallet_id'] in onlineClients:            
+                    w.append(x['handle'])
+      data = { 'handles': list(w) }
+      return data
+
+    @dispatcher.add_method
     def is_chat_handle_in_use(handle):
         results = mongo_db.chat_handles.find({ 'handle': { '$regex': '^%s$' % handle, '$options': 'i' } })
         return True if results.count() else False
